67d95ed35044daba5a111f2622057009b4fc95a2
William Ughetta <wughetta@alumni.princeton.edu>
2022-02-17 00:56:15 -0500

    Add max run time. Remove color bold. Fix execution

--- par.sh
+++ par.sh
@@ -9,18 +9,21 @@ set +euf -o pipefail
 if [ $# -lt 1 ]; then >&2 echo example: $0 \'ping\ {google,apple}.com\'; exit 1; fi
 
 TAG='par'
+TIMEOUT_MINUTES=0 # Max run time (0 == DISABLED)
 
-# Print colored line (6 colors: red, green, yellow, purple, pink, cyan). Default color: 39
+# Print colored line (6 colors: red, green, yellow, purple, pink, cyan)
 # https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
 function print_color {
-  printf '\033[1;%sm%s\033[0m\n' $(( $1 ? 31+$1%6 : 39 )) "$2"
+  COLOR_BEGIN='\033[1;%sm'; COLOR_END='\033[0m'
+  if [ $1 -eq 0 ]; then printf "${COLOR_END}%s\n" "$2"; else
+    printf "${COLOR_BEGIN}%s${COLOR_END}\n" $(( 31+$1%6 )) "$2"; fi
 }
 
 # Line up the output names
-ABSMAX=71
+ABSMAX=72
 MAXLEN=0; for i; do if [ ${#i} -gt $MAXLEN ]; then MAXLEN=${#i}; fi; done
 if [ $MAXLEN -gt $ABSMAX ]; then MAXLEN=$ABSMAX; fi
-function println { print_color $1 "$(printf "%-$(($MAXLEN+1))s| %s" "$2" "$3")" ; }
+function println { print_color $1 "$(printf "%-$(($MAXLEN))s| %s" "$2" "$3")" ; }
 
 # Pad: Adapted from http://catern.org/pipes.html
 PIPEBUF=4096 #`getconf PIPE_BUF /` # Reduces interleaving
@@ -32,15 +35,16 @@ UNIFIED=`mktemp -u -t PIPE_UNIFIED`; mkfifo $UNIFIED
 PIDS=""; C=0
 for i; do
   C=$(( $C + 1 )); t=`mktemp -u -t PIPE_TMP_$C`; mkfifo $t
-  eval "${i} &> $t &"
+  ${i} &> $t &
   PIDS+="$! "
   cat $t | while read line; do println $C "$i" "$line" ; done | pad >> $UNIFIED &
 done
 println 0 "$TAG" "Started $C procs. Waiting for any nonzero exit. PIDs: $PIDS"
 
-
-# Output stream. Prevents lines (< 4096 chars) from interleaving
-# TODO: Improvement detect no output for TIMEOUT (i.e. 5 minutes) and halt
+# Max run time
+if [[ $TIMEOUT_MINUTES -ne 0 ]]; then
+  ( sleep $(( $TIMEOUT_MINUTES * 5 )) && echo "$PIDS" | xargs -n1 kill -9 ; println 0 "$TAG" "Timeout: $TIMEOUT_MINUTES minutes. Exit code: 1"; exit 1 ) &
+fi
 
 EXITCODE=0
 N_EXIT_ZERO=0
